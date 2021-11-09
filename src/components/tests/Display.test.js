5375e9f1bf43aee8a517dc3a82610da67a2e9325
ModupeD <bimisoladaniel@Bimisolas-MacBook-Air.local[credential]>
2021-11-09 17:29:19 +0100

    Making progress

--- src/components/tests/Display.test.js
+++ src/components/tests/Display.test.js
@@ -1,5 +1,67 @@
-test("renders without error", () => {});
+import React from "react";
+import { render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
+import Display from "./../Display";
+
+import mockFetchShow from "./../../api/fetchShow";
+jest.mock("./../../api/fetchShow");
+
+const testShow = {
+  name: "",
+  summary: "",
+  seasons: [
+    {
+      id: 0,
+      name: "Season 1",
+      episodes: [],
+    },
+    {
+      id: 1,
+      name: "Season 2",
+      episodes: [],
+    },
+  ],
+};
+
+test("renders without error", () => {
+  render(<Display />);
+});
+
+test("renders Show component when the button is clicked", async () => {
+  mockFetchShow.mockResolvedValueOnce(testShow);
+  render(<Display />);
+  const button = screen.getByRole("button");
+  userEvent.click(button);
+
+  const show = await screen.findByTestId("show-container");
+  expect(show).toBeInTheDocument();
+});
+
+test("renders season options matching fetch return when button is clicked", async () => {
+  mockFetchShow.mockResolvedValueOnce(testShow);
+  render(<Display />);
+  const button = screen.getByRole("button");
+  userEvent.click(button);
+
+  await waitFor(() => {
+    const seasonOptions = screen.queryAllByTestId("season-option");
+    expect(seasonOptions).toHaveLength(2);
+  });
+});
+
+test("displayFunc is called when the fech button pressed", async () => {
+  mockFetchShow.mockResolvedValueOnce(testShow);
+
+  const displayFunc = jest.fn();
+  render(<Display displayFunc={displayFunc} />);
+  const button = screen.getByRole("button");
+  userEvent.click(button);
+
+  await waitFor(() => {
+    expect(displayFunc).toHaveBeenCalled();
+  });
+});
 ///Tasks:
 //1. Add in nessisary imports and values to establish the testing suite.
 //2. Test that the Display component renders without any passed in props.
